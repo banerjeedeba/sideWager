f96930db4b6e63c40a21d949ada32b6dc0d45d68
rajibgiri <giri.rajib@hotmail.com>
2017-07-09 21:01:59 -0400

    prod firebase setup

--- SideWager/WebContent/resources/sw/swApp/src/environments/environment.prod.ts
+++ SideWager/WebContent/resources/sw/swApp/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDtC8Ci-th7n9dnLRtqFIPUlY0B6LRjht8",
+    authDomain: "sidewager-163802.firebaseapp.com",
+    databaseURL: "https://sidewager-163802.firebaseio.com",
+    projectId: "sidewager-163802",
+    storageBucket: "sidewager-163802.appspot.com",
+    messagingSenderId: "1050634895535"
+  }
 };
