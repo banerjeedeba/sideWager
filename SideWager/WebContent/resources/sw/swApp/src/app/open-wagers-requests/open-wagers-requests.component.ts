34e12b38ff07394e7c7b181354dac4f7a04ac240
rajibgiri <giri.rajib@hotmail.com>
2017-08-11 00:06:17 -0400

    open wager request new UI implemented

--- SideWager/WebContent/resources/sw/swApp/src/app/open-wagers-requests/open-wagers-requests.component.ts
+++ SideWager/WebContent/resources/sw/swApp/src/app/open-wagers-requests/open-wagers-requests.component.ts
@@ -11,6 +11,9 @@ export class OpenWagersRequestsComponent implements OnInit {
 
   ngOnInit() {
   }
+
+
+  
 public listFrinds=[
   {
     "Name":"Brent Boner",
@@ -34,4 +37,32 @@ public listFrinds=[
     "Amount":"650"
   },
 ];
+
+public gamesList=[{
+  "Team1FullName":"Toranto",
+  "Team2FullName":"Chicago",
+  "Team1Name":"TOR",
+  "Team2Name":"CHI",
+  "Team1Ind":"TOR-6.5",
+  "Team2Ind":"u 102.5",
+  "Time":"Aug 15 8:00PM",
+},
+{
+  "Team1FullName":"Cavaliers",
+  "Team2FullName":"Minnesota",
+  "Team1Name":"CLE",
+  "Team2Name":"MIN",
+  "Team1Ind":"CLE-4.5",
+  "Team2Ind":"u 105.5",
+  "Time":"Sep 10 8:00PM",
+},
+{
+  "Team1FullName":"Sacremento",
+  "Team2FullName":"Los Angeles",
+  "Team1Name":"SAC",
+  "Team2Name":"LAL",
+  "Team1Ind":"TOR-6.5",
+  "Team2Ind":"u 101",
+  "Time":"Oct 17 10:30PM",
+}];
 }
